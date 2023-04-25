29bb673910f5c4d34d447a7f5781820ee89c8ee6
Andrea Perin <andrea.perin1992@gmail.com>
2023-04-25 14:40:20 +0200

    Add installation script for mac of vim firacode and zsh with brew

---
+++ darwin/dependences.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+## Installing Fira Code
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code
+
+# Installing Zsh
+brew install zsh 
+
+# Installing Vim
+brew install vim
+
+echo "Dependencies succesfully installed"
