7fac616643fe2e91f353cb00267cdee721b7c8ae
Andrea Perin <andrea.perin1992@gmail.com>
2023-10-18 10:06:02 +0200

    Change lsd shortcut to ll.

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -32,12 +32,12 @@ if (Get-Command ls.exe -ErrorAction SilentlyContinue | Test-Path) {
     # List all files in long format, including hidden files
     ${function:la} = { ls -laF @args }
     # List only directories
-    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+    ${function:ll} = { Get-ChildItem -Directory -Force @args }
 } else {
     # List all files, including hidden files
     ${function:la} = { ls -Force @args }
     # List only directories
-    ${function:lsd} = { Get-ChildItem -Directory -Force @args }
+    ${function:ll} = { Get-ChildItem -Directory -Force @args }
 }
 
 # curl: Use `curl.exe` if available
