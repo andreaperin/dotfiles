9a8bdaae1d3b187caedb36a1d352077827c5aa17
Andrea Perin <andrea.perin1992@gmail.com>
2023-11-20 20:04:52 +0100

    Add todo for custom pwsh oh-my-posh template.

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -4,3 +4,5 @@
 Push-Location (Split-Path -parent $profile)
 "components","functions","aliases" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
 Pop-Location
+
+##TODO use a custom template of oh-my-posh for adding julia and conda section in the prompt
