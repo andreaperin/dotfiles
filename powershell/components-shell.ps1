14fa5147886131d5e215638de63516165fc35497
Andrea Perin <andrea.perin1992@gmail.com>
2023-11-21 11:29:24 +0100

    Delete 2 not used files components-shell and components

--- powershell/components-shell.ps1
+++
@@ -1,8 +0,0 @@
-# # These components will be loaded when running Microsoft.Powershell (i.e. Not Visual Studio)
-
-# Push-Location (Join-Path (Split-Path -parent $profile) "components")
-
-# # From within the ./components directory...
-# . .\console.ps1
-
-# Pop-Location
