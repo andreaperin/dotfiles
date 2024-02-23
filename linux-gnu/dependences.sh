e5af1148f61a82ffaae697602c6debaf78ee31c0
Andrea Perin <andrea.perin1992@gmail.com>
2024-02-23 15:16:15 +0100

    Add everything for ubuntu/WSL (to test)

--- linux-gnu/dependences.sh
+++ linux-gnu/dependences.sh
@@ -18,6 +18,24 @@ sudo apt install alacritty -y
 # Installing starship
 sudo apt install starship -y
 
+# Installing fd
+sudo apt install fd-find
+
+# Installing fzf
+sudo apt install fzf
+
+# Installing zoxide
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+# Installing tmux
+sudo apt install tmux
+
+# Installing lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
 sudo apt autoremove -y
 
 echo "Dependencies succesfully installed"
